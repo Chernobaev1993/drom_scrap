3e915ecb12c900e82c8e2b8b9ca9ef62455f1165
Aleksandr Chernobaev <chernobaevap1993@gmail.com>
2023-04-18 12:35:47 +0500

    All-page scrap

--- drombs4.py
+++ drombs4.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 city = 'tyumen'
 domain = f'https://{city}.drom.ru/auto'
-start_url = f'{domain}/all/?maxprice=500000&pts=2&w=2&unsold=1'
+start_url = 'https://auto.drom.ru/region72/suv/all/?maxprice=1300000&inomarka=1&pts=2&damaged=2&w=2&unsold=1'
 
 
 class Car:
@@ -20,10 +20,15 @@ class Car:
         return self.characters
 
 
-def get_div_list(url):
+def get_main_div(url):
     get_html = requests.get(url)
     soup = BeautifulSoup(get_html.content, "lxml")
-    div_list = soup.find('div', class_='css-1nvf6xk eojktn00').find_all('a', class_='css-xb5nz8 e1huvdhj1')
+    main_div = soup.find('div', class_='css-1nvf6xk eojktn00')
+    return main_div
+
+
+def get_car_list(div):
+    div_list = div.find_all('a', class_='css-xb5nz8 e1huvdhj1')
     return div_list
 
 
@@ -45,7 +50,11 @@ def get_car(div):
         # run = characters[4].text.replace(',', '')
 
         price = div.find('span', class_='css-46itwz e162wx9x0').text
-        price_grade = div.find('div', class_='css-11m58oj evjskuu0').text
+        price_grade = div.find('div', class_='css-11m58oj evjskuu0')
+        if not price_grade:
+            price_grade = 'без оценки'
+        else:
+            price_grade = price_grade.text
 
         car = Car(
             link=link,
@@ -63,6 +72,18 @@ def get_car(div):
         time.sleep(20)
 
 
-for i in get_div_list(start_url):
-    get_car(i).print_car()
+count = 1
+main_div = get_main_div(start_url)
+while main_div:
+    for i in get_car_list(main_div):
+        print(count, end='. ')
+        get_car(i).print_car()
+        count += 1
+    next_page_link = main_div.find('a', class_='css-4gbnjj e24vrp30')
+    if next_page_link:
+        next_page_link = next_page_link['href']
+        main_div = get_main_div(next_page_link)
+    else:
+        main_div = None
 
+# next_page = get
